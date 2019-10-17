b19719b423fabe6d9491c076c3ec0c3a53c76200
Mauricio <mauricioferreyra548@gmail.com>
2019-10-17 16:46:22 -0300

    registro funcional,falta info de error

--- FeriaCultiva/apps/user/views.py
+++ FeriaCultiva/apps/user/views.py
@@ -3,16 +3,15 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import User
+from apps.user.forms import RegistroForm
+
 
 # Create your views here.
 
 class CreateUser(CreateView):
     model = User
     template_name = 'user/registro.html'
+    #form_class = RegistroForm
     succes_url = reverse_lazy('home.html')
-    fields = ['username','password', 'email', 'first_name', 'last_name', 'direccion', 'telefono']
+    fields = ['username','first_name','last_name','email','password','direccion']
 
-class UpdateUser(UpdateView):
-    model = User
-    template_name = ''
-    fields = ['password', 'email', 'first_name', 'last_name', 'direccion', 'telefono']
