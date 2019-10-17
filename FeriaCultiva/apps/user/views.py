bb818e9f5ee2954713d2bed27677f2b9fb80af8e
Mauricio <mauricioferreyra548@gmail.com>
2019-10-16 23:37:44 -0300

    Avance en el registro backend

--- FeriaCultiva/apps/user/views.py
+++ FeriaCultiva/apps/user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import User
 
 # Create your views here.
+
+class CreateUser(CreateView):
+    model = User
+    template_name = 'user/registro.html'
+    # succes_url = reverse_lazy('usuario:home')
+    fields = '__all__'
+
+class UpdateUser(UpdateView):
+    model = User
+    template_name = ''
+    fields = ['password', 'email', 'first_name', 'last_name', 'direccion', 'telefono']
