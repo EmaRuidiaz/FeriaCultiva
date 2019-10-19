ac2f898d2e52ebe60acde07bc21230da5d28e8b3
EmaRuidiaz <ema_ruidiaz@hotmail.com>
2019-10-19 02:42:33 -0300

    actualizado con base

--- FeriaCultiva/apps/producto/urls.py
+++ FeriaCultiva/apps/producto/urls.py
@@ -6,7 +6,7 @@ app_name = "producto"
 
 urlpatterns = [
     path('agregar/', views.AgregarProducto.as_view(), name="agregar"),
-    path('productos/', views.ListarProductos.as_view(), name="listar"),
+    path('', views.ListarProductos.as_view(), name="listar"),
     path('detalle/<int:pk>', views.DetalleProducto.as_view(), name="detalle"),
     path('modificar/<int:pk>', views.ModificarProducto.as_view(), name="modificar"),
     path('eliminar/<int:pk>', views.EliminarProducto.as_view(), name="eliminar"),
