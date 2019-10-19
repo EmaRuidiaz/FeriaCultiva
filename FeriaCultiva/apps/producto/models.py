a8b38715a1ddaee1cb68e88bd2ce9cb3e1aa9b13
EmaRuidiaz <ema_ruidiaz@hotmail.com>
2019-10-19 03:18:49 -0300

    limpiando

--- FeriaCultiva/apps/producto/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from apps.categoria.models import Categoria
-# Create your models here.
-class Producto(models.Model):
-	nombre = models.CharField(max_length=30)
-	precio = models.DecimalField(max_digits=8, decimal_places=2)
-	stock = models.IntegerField()
-	foto_producto = models.ImageField(upload_to = 'productos', null = True)
-	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productosXcategoria')
