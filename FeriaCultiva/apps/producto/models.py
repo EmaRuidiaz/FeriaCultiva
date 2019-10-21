5e1da696ac2d44ff2cdb85edd91fcbe19e30d8c9
Mauricio <mauricioferreyra548@gmail.com>
2019-10-21 20:36:23 -0300

    detalle de producto

--- FeriaCultiva/apps/producto/models.py
+++ FeriaCultiva/apps/producto/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.categoria.models import Categoria
+from apps.feriante.models import Feriante
 # Create your models here.
 class Producto(models.Model):
 	nombre = models.CharField(max_length=30)
@@ -7,3 +8,4 @@ class Producto(models.Model):
 	stock = models.IntegerField()
 	foto_producto = models.ImageField(upload_to = 'productos', null = True)
 	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productosXcategoria')
+	feriante = models.ForeignKey(Feriante, on_delete=models.CASCADE, related_name='productosXferiante')
--- FeriaCultiva/apps/producto/views.py
+++ FeriaCultiva/apps/producto/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Producto
+
+class DetalleProducto(DetailView):
+    model = Producto
+    template_name = 'producto/detalle_producto.html'
